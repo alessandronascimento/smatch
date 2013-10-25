d31f0ccd9581574a626bbb308d049c2aa51c9d26
al.s.nascimento <al.s.nascimento@gmail.com>
2013-10-25 18:52:42 +0000

    Major changes to the following classes: * Parser * Printer and * Mol
    Classes that sill have to be fixed:
    * Optimization
    * Coord and
    the main file.

--- SMatch/src/Parser.cpp
+++ SMatch/src/Parser.cpp
@@ -8,11 +8,41 @@
 #include "Parser.h"
 
 
-Parser::Parser() {
-	// TODO Auto-generated constructor stub
+Parser::Parser(char* arg) {
+	search_radius=5.0;
+	ifstream input(arg);
+	char line[256];
+	while (!input.eof()){
+		input >> param;
+		if ((param.substr(0,1) == "#") or (param.substr(0,2) == "//")){
+			input.getline(line, 256);
+		}
+		else{
+			this->parse_param(param, input);
+		}
+	}
+	input.close();
+}
 
+
+void Parser::parse_param(string param, ifstream &input){
+	int res;
+	string resname;
+	if (param == "reference_file"){
+		input >> this->reference_file;
+	}
+	else if (param == "multifile"){
+		input >> this->multifile;
+	}
+	else if (param == "search"){
+		input >> res >> resname;
+		this->selected_residues.push_back(res);
+		this->residue_types.push_back(resname);
+	}
+	else if (param == "search_radius"){
+		input >> this->search_radius;
+	}
 }
 
 Parser::~Parser() {
-	// TODO Auto-generated destructor stub
 }
--- SMatch/src/SMatch.cpp
+++ SMatch/src/SMatch.cpp
@@ -12,121 +12,71 @@
 #include "Optimization.h"
 #include "SMatch.h"
 #include "Printer.h"
+#include "Parser.h"
 
 Mol* Optimization::M1;
 
-void mol_extraction(Mol* M1, Mol* MExtract1, char* argv[], int number_of_selected_res){
-	int resn;
-	for (int i=0; i< number_of_selected_res; i++){
-		resn = atoi(argv[2+i]);
-		resn = resn-M1->resnumber_phase;
-		MExtract1->resnames.push_back(M1->resnames[resn]);
-		MExtract1->residue_pointer.push_back(int(MExtract1->xyz.size()));
-		printf("Residue %d selected (%s): %dth residue\n", atoi(argv[2+i]), M1->resnames[resn].c_str(), resn);
-			for (int j=M1->residue_pointer[resn]; j<M1->residue_pointer[resn+1]; j++){
-				MExtract1->xyz.push_back(M1->xyz[j]);
-				MExtract1->atomnames.push_back(M1->atomnames[j]);
+void mol_extraction(Mol* M1, Mol* MExtract1, Parser* Input){
+	for (unsigned i=0; i<Input->selected_residues.size(); i++){
+		for (unsigned j=0; j< M1->mymol.size(); j++){
+			if (M1->mymol[j].resnumber == Input->selected_residues[i]){
+				MExtract1->mymol.push_back(M1->mymol[j]);
 			}
 		}
-	MExtract1->N = int(MExtract1->xyz.size());
-	MExtract1->Nres = int(MExtract1->resnames.size());
-	MExtract1->filename = M1->filename;
-//	MExtract1->charge_distribution();
-
-#ifdef DEBUG
-	for (unsigned i=0; i<MExtract1->resnames.size()-1; i++){
-		printf("%s\n", MExtract1->resnames[i].c_str());
-		for (int j=MExtract1->residue_pointer[i]; j<MExtract1->residue_pointer[i+1]; j++){
-			printf("Atom %5s   %8.3f %8.3f %8.3f    %8.3f\n", MExtract1->atomnames[j].c_str(), MExtract1->xyz[j][0], MExtract1->xyz[j][1], MExtract1->xyz[j][2], MExtract1->charges[j]);
-		}
 	}
-	printf("%s\n", MExtract1->resnames[MExtract1->resnames.size()-1].c_str());
-	int j=MExtract1->residue_pointer[MExtract1->resnames.size()-1];
-	while (j<MExtract1->N){
-		printf("Atom %5s   %8.3f %8.3f %8.3f    %8.3f\n", MExtract1->atomnames[j].c_str(), MExtract1->xyz[j][0], MExtract1->xyz[j][1], MExtract1->xyz[j][2], MExtract1->charges[j]);;
-		j++;
-	}
-#endif
 }
 
 void mol_extraction(Mol* M1, Mol* MExtract1, string resname){
-	int start_atom, end_atom;
-	int rpointer=0;
-	for (unsigned i=0; i< M1->resnames.size(); i++){
-		if (M1->resnames[i] == resname){
-
-			MExtract1->resnames.push_back(M1->resnames[i]);
-			start_atom = M1->residue_pointer[i];
-			if (i < M1->resnames.size()-1){
-				end_atom = M1->residue_pointer[i+1];
-			}
-			else{
-				end_atom = M1->N;
-			}
-			
-			MExtract1->residue_pointer.push_back(rpointer);
-			for (int j=start_atom; j<end_atom; j++){
-				MExtract1->xyz.push_back(M1->xyz[j]);
-				MExtract1->atomnames.push_back(M1->atomnames[j]);
-				rpointer++;
-			}
-//#ifdef DEBUG
-			printf("%3d -  residue %s [%d]\n", MExtract1->resnames.size()-1, M1->resnames[i].c_str(), i+M1->resnumber_phase);
-//#endif
+	for (unsigned i=0; i<M1->mymol.size(); i++){
+		if (M1->mymol[i].resname == resname){
+			MExtract1->mymol.push_back(M1->mymol[i]);
 		}
 	}
+}
 
-	MExtract1->N = int(MExtract1->xyz.size());
-	MExtract1->Nres = int(MExtract1->resnames.size());
-	MExtract1->filename = M1->filename;
-//	MExtract1->charge_distribution();
+vector<string> make_unique(vector<string> resnames){
+	vector<string> unique;
+	unique.push_back(resnames[0]);
+	bool found;
+	for (unsigned i=1; i<resnames.size(); i++){
+		found=false;
+		for (unsigned j=0; j< unique.size(); j++){
+			if (resnames[i] == unique[j]){
+				found = true;
+			}
+		}
+		if (!found){
+			unique.push_back(resnames[i]);
+		}
+	}
+	return(unique);
 }
 
 int main(int argc, char* argv[]) {
 
-	if (argc < 2){
-		printf ("Usage: %s structure.pdb res1 res2 res3.\n", argv[0]);
+	if (argc != 2){
+		printf ("Usage: %s input_file.\n", argv[0]);
 		exit(1);
 	}
 
-	Mol* M1 = new Mol;
-	M1->read_pdb(string (argv[1]));
+	Parser* Input = new Parser(argv[1]);
 
-	int number_of_selected_res= argc-2;
+	Mol* M1 = new Mol;
+	M1->read_pdb(Input->reference_file);
+
+	int number_of_selected_res= int(Input->selected_residues.size());
 	printf("%d residues were selected for structure extraction...\n", number_of_selected_res);
 
 	Mol* MExtract1 = new Mol;
-	mol_extraction(M1, MExtract1, argv, number_of_selected_res);
+	mol_extraction(M1, MExtract1, Input);
 
-	Coord* CoordManip = new Coord;
-
-	vector<string> res_types;
-	bool not_found = false;
-	res_types.push_back(MExtract1->resnames[0]);
-	for (unsigned i =0; i< MExtract1->resnames.size(); i++){
-		not_found = false;
-		for (unsigned j=0; j< res_types.size(); j++){
-			if (MExtract1->resnames[i] == res_types[j]){
-				not_found = true;
-			}
-		}
-		if (! not_found){
-			res_types.push_back(MExtract1->resnames[i]);
-		}
-	}
-
-#ifdef DEBUG
-	printf("List of residues to look for:\n");
-	for (unsigned i=0; i< res_types.size(); i++){
-		printf("\t%s\n", res_types[i].c_str());
-	}
-#endif
+	vector<string> unique = make_unique(Input->residue_types);
 
 	Printer* Writer = new Printer;
-	Writer->write_pdb(MExtract1, MExtract1->xyz, 0.0, 0.0, "ME1");
+	Writer->write_pdb(MExtract1, 0.0, 0.0, "ME1");
 
 	ifstream multifile;
-	multifile.open("INSMATCH");
+	multifile.open(Input->multifile.c_str());
 	string pdbmol;
 	multifile >> pdbmol;
 
@@ -134,14 +84,14 @@ int main(int argc, char* argv[]) {
 		Mol* M2 = new Mol;
 		Mol* MExtract2 = new Mol;
 		M2->read_pdb(pdbmol);
-		for (unsigned i=0; i< res_types.size(); i++){
-			mol_extraction(M2, MExtract2, res_types[i]);
+		for (unsigned i=0; i< unique.size(); i++){
+			mol_extraction(M2, MExtract2, unique[i]);
 		}
 
-		printf("Found %d matching residues in search molecule.\n", int(MExtract2->resnames.size()));
+		printf("Found %d matching residues in search molecule.\n", int(MExtract2->mymol.size()));
 
 		vector<vector<double> > xyz;
-		Optimization* Opt = new Optimization(Writer, MExtract1);
+/*		Optimization* Opt = new Optimization(Writer, MExtract1);
 		opt_result_t* opt_result = new opt_result_t;
 		Opt->optimize_rmsd(MExtract2, opt_result);
 
@@ -160,9 +110,12 @@ int main(int argc, char* argv[]) {
 	delete Writer;
 	delete MExtract1;
 	delete M1;
-
+*/
+	}
 	return EXIT_SUCCESS;
 }
+
+
 
 
 //TODO: Alter SMatch.cpp for the new definition of a molecule in Mol class.
