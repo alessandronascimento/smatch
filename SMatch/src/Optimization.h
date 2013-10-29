9802eb4691e01d7268e7c1a5977feebcc40deb1a
al.s.nascimento <al.s.nascimento@gmail.com>
2013-10-29 15:59:08 +0000

    First parallel (MPI) version of SMatch

--- SMatch/src/Optimization.h
+++ SMatch/src/Optimization.h
@@ -18,23 +18,23 @@
 
 class Optimization {
 public:
+
 	static Mol* M1;
 	Printer* Writer;
 	char info[98];
 	Optimization(Printer* _Writer, Mol* _M1);
 	static double dist_squared(double x1, double x2, double y1, double y2, double z1, double z2);
-//	static double objective_overlay_function(const std::vector<double> &x, std::vector<double> &grad, void *data);
 	static double pre_optimize_rmsd_function(const std::vector<double> &x, std::vector<double> &grad, void *data);
 	void minimize_overlay_nlopt_ln_auglag(Mol* M2);
     vector<vector<vector<double> > >update_coords(const std::vector<double> &x, Mol* M2);
 	virtual ~Optimization();
     static double compute_rmsd(Mol* M1, Mol* M2, vector<vector<vector<double> > >xyz, int r1, int r2);
 	void optimize_rmsd(Mol* M2, opt_result_t* opt_result);
-
 	struct opt_data{
 		Mol* M2;
 		int resnumber;
 	};
+
 };
 
 #endif /* OPTIMIZATION_H_ */
--- SMatch/src/SMatch.h
+++ SMatch/src/SMatch.h
@@ -8,14 +8,11 @@
 #ifndef SMATCH_H_
 #define SMATCH_H_
 
-#include <vector>
-
 struct opt_result_t {
-	bool succeded;
-	vector<double> rotrans;
-    vector<vector<vector<double> > >xyz;
-	double rmsd;
-};
-
+		bool succeded;
+		vector<double> rotrans;
+	    vector<vector<vector<double> > >xyz;
+		double rmsd;
+	};
 
 #endif /* SMATCH_H_ */
